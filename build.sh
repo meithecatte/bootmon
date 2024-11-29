35e1306ca33e07b545a411c68385437b9a054dfe
sdomi <ja@sdomi.pl>
2024-11-29 04:16:23 +0100

    build: busybox compat

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 nasm -f bin bootmon.asm -o bootmon.bin
-bytecount=$(xxd -ps bootmon.bin |
+bytecount=$(xxd -p bootmon.bin |
     tr -d '\n' |
     sed -E 's/(00)*55aa$//g' |
-    xxd -ps -r |
+    xxd -p -r |
     wc -c)
 echo "$bytecount bytes used"
